30b3a2a8981e47c29a95b5f45bbd5319e54c2153
ivex0002 <garam199803@gmail.com>
2026-01-29 16:08:34 +0900

    fix:annotation

--- src/factory_pattern_api/axiosInstances.ts
+++ src/factory_pattern_api/axiosInstances.ts
@@ -5,10 +5,10 @@ const BASE_URL = "https://oz.example.com/";
 const EXTERNAL_URL = "https://external.api.com/";
 
 // 토큰 매니저 생성
-// 인스턴스들은 모두 동일한 토큰을 참조하도록 보장
 const tokenManager = new TokenManager();
 
 // 인스턴스들을 생성하는 클래스(팩토리 패턴)
+// 해당 클래스로 생성되는 인스턴스들은 모두 동일한 토큰을 참조하도록 보장
 const apiFactory = new AxiosInstanceFactory(tokenManager);
 
 // 팩토리를 통해 생성된 인스턴스들
